b64d41d428039ae624526cfe8843c37e97d8f6ea
sambabhouria <sambabhouria@gmail.com>
2023-03-04 14:21:31 +0100

    client side init

--- server/config.js
+++ server/config.js
@@ -4,7 +4,7 @@ config();
 export const MONGODB_URI =
   process.env.MONGODB_URI ||
   "mongodb+srv://userdatabase:bhouriamongodbatlas123@shoeshop.0ybin.mongodb.net/tododemo?retryWrites=true&w=majority";
-export const PORT = process.env.PORT;
+export const PORT = process.env.PORT || 4000;
 
 export const CLOUD_NAME = process.env.CLOUD_NAME;
 export const API_KEY = process.env.API_KEY;
