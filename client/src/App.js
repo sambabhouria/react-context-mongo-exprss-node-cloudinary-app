b4dacdda3c902cbc465d359908592ef017831611
sambabhouria <sambabhouria@gmail.com>
2023-03-04 14:36:14 +0100

    client side added

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { Toaster } from "react-hot-toast";
+import { Route, Routes } from "react-router-dom";
+import { PostProvider } from "./context/postContext";
+import { HomePage, NotFoundPage, PostForm } from "./pages";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-neutral-900 min-h-screen flex items-center">
+      <div className="px-10 container m-auto py-4">
+        <PostProvider>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/new" element={<PostForm />} />
+            <Route path="/:id" element={<PostForm />} />
+            <Route path="*" element={<NotFoundPage />} />
+          </Routes>
+          <Toaster />
+        </PostProvider>
+      </div>
     </div>
   );
 }
---
+++ client/src/pages/index.js
@@ -0,0 +1,3 @@
+export { HomePage } from "./HomePage";
+export { NotFoundPage } from "./NotFoundPage";
+export { PostForm } from "./PostForm";
